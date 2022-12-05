a0dec0a0c66336a54ea8e7c88cea57eee0608702
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2022-12-04 22:10:40 -0300

    feat: add color helpers

---
+++ lib/src/color_helper.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+extension ColorHelper on Color {
+  /// This helper will convert the string to a color object. It receives a hex
+  /// string in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  /// This helper will convert the color object to a hex string.
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
+
+  /// This helper will darken the color object by the factor of [amount]
+  /// (ranges from 0 to 1).
+  Color darken([double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+    final hsl = HSLColor.fromColor(this);
+    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+    return hslDark.toColor();
+  }
+
+  /// This helper will lighten the color object by the factor of [amount]
+  /// (ranges from 0 to 1).
+  Color lighten([double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+    final hsl = HSLColor.fromColor(this);
+    final hslLight =
+        hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+    return hslLight.toColor();
+  }
+}
